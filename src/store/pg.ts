a3657f0184867b9eb9443b24835ea0cbb46800b0
anabiz <anabizconcept9@gmail.com>
2021-02-14 12:15:46 +0100

    made changes

--- src/store/pg.ts
+++ src/store/pg.ts
@@ -4,7 +4,7 @@ import createConnectionPool, { sql } from '@databases/pg';
 // N.B. you will need to replace this connection
 // string with the correct string for your database.
 const db = createConnectionPool(
-  process.env.DATABASE_URL_MAIN
+  process.env.DATABASE_URL
 );
 
 
